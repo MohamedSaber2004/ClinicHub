72a98f2879590375c6f0afed46512205354fd185
Mohamed Saber <mmms77990@gmail.com>
2026-05-22 18:17:59 +0300

    savechanges

--- ClinicHub.Application/Features/RealTime/Commands/SetActiveConversation/SetActiveConversationCommandValidator.cs
+++ ClinicHub.Application/Features/RealTime/Commands/SetActiveConversation/SetActiveConversationCommandValidator.cs
@@ -1,4 +1,5 @@
-﻿using ClinicHub.Domain.Entities;
+﻿using ClinicHub.Application.Localization;
+using ClinicHub.Domain.Entities;
 using ClinicHub.Infrastructure.UnitOfWork.Interfaces;
 using FluentValidation;
 using Microsoft.Extensions.Localization;
@@ -14,8 +15,8 @@ namespace ClinicHub.Application.Features.RealTime.Commands.SetActiveConversation
             _ctx = ctx;
 
             RuleFor(x => x.ConversationId)
-                .NotEmpty().WithMessage(localizer["RealTime:ConversationIdRequired"])
-                .MustAsync(ConversationFound).WithMessage(localizer["RealTime:ConversationNotFound"]);
+                .MustAsync(ConversationFound).WithMessage(localizer["RealTime:ConversationNotFound"])
+                .When(c => c.ConversationId is not null);
         }
 
         private async Task<bool> ConversationFound(Guid? conversationId, CancellationToken cancellationToken)
