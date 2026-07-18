af7c0c00f6359920b55de004253412889eed641b
Mohamed Saber <mmms77990@gmail.com>
2026-07-18 17:18:37 +0300

    savechanges

--- ClinicHub.Application/Features/Doctors/DoctorProfile.cs
+++ ClinicHub.Application/Features/Doctors/DoctorProfile.cs
@@ -15,7 +15,8 @@ namespace ClinicHub.Application.Features.Doctors
                 .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User != null ? src.User.FullName : null))
                 .ForMember(dest => dest.UserEmail, opt => opt.MapFrom(src => src.User != null ? src.User.Email : null))
                 .ForMember(dest => dest.ClinicName, opt => opt.MapFrom(src => src.Clinic != null ? src.Clinic.Name : null))
-                .ForMember(dest => dest.SpecializationName, opt => opt.MapFrom(src => src.Specialization != null ? src.Specialization.Name : null));
+                .ForMember(dest => dest.SpecializationName, opt => opt.MapFrom(src => src.Specialization != null ? src.Specialization.Name : null))
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.IsActive && !src.IsDeleted));
         }
     }
 }
