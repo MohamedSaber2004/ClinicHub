e2a2e24b3de364c088c95ce37bf2e663bc88a542
Mohamed Saber <mmms77990@gmail.com>
2026-07-18 17:47:41 +0300

    update edit user endpoint business

--- ClinicHub.Application/Features/Users/Commands/EditUser/EditUserCommandHandler.cs
+++ ClinicHub.Application/Features/Users/Commands/EditUser/EditUserCommandHandler.cs
@@ -1,8 +1,11 @@
 using ClinicHub.Application.Common.Exceptions;
+using ClinicHub.Application.Common.Interfaces;
 using ClinicHub.Application.Localization;
 using ClinicHub.Domain.Entities;
+using ClinicHub.Domain.Enums;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
 
 namespace ClinicHub.Application.Features.Users.Commands.EditUser
@@ -10,13 +13,16 @@ namespace ClinicHub.Application.Features.Users.Commands.EditUser
     public class EditUserCommandHandler : IRequestHandler<EditUserCommand, bool>
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IClinicHubContext _context;
         private readonly IStringLocalizer<Messages> _localizer;
 
         public EditUserCommandHandler(
             UserManager<ApplicationUser> userManager,
+            IClinicHubContext context,
             IStringLocalizer<Messages> localizer)
         {
             _userManager = userManager;
+            _context = context;
             _localizer = localizer;
         }
 
@@ -36,6 +42,28 @@ namespace ClinicHub.Application.Features.Users.Commands.EditUser
             {
                 var errors = string.Join(", ", result.Errors.Select(e => e.Description));
                 throw new BadRequestException(errors);
+            }
+
+            if (request.IsActive.HasValue)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+
+                if (roles.Any(r => r == nameof(UserType.Doctor) || r == nameof(UserType.ClinicOwner)))
+                {
+                    var doctor = await _context.Doctors
+                        .IgnoreQueryFilters()
+                        .FirstOrDefaultAsync(d => d.UserId == user.Id, cancellationToken);
+
+                    if (doctor is not null)
+                    {
+                        if (request.IsActive.Value)
+                            doctor.Active();
+                        else
+                            doctor.Deactive();
+
+                        await _context.SaveChangesAsync(cancellationToken);
+                    }
+                }
             }
 
             return true;
