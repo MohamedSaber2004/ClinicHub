b1ad0c2e2a17e591a8644e75467f573cb900744d
Mohamed Saber <mmms77990@gmail.com>
2026-07-18 15:20:36 +0300

    savechanges

--- ClinicHub.Application/Features/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
+++ ClinicHub.Application/Features/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -1,17 +1,24 @@
 using ClinicHub.Application.Common.Exceptions;
+using ClinicHub.Application.Common.Interfaces;
 using ClinicHub.Domain.Entities;
+using ClinicHub.Domain.Enums;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClinicHub.Application.Features.Users.Commands.DeleteUser
 {
     public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IClinicHubContext _context;
+        private readonly ICurrentUserService _currentUserService;
 
-        public DeleteUserCommandHandler(UserManager<ApplicationUser> userManager)
+        public DeleteUserCommandHandler(UserManager<ApplicationUser> userManager, IClinicHubContext context, ICurrentUserService currentUserService)
         {
             _userManager = userManager;
+            _context = context;
+            _currentUserService = currentUserService;
         }
 
         public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
@@ -32,6 +39,21 @@ namespace ClinicHub.Application.Features.Users.Commands.DeleteUser
                 var errors = string.Join(", ", result.Errors.Select(e => e.Description));
                 throw new BadRequestException(errors);
             }
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            if (roles.Any(r => r == nameof(UserType.Doctor) || r == nameof(UserType.ClinicOwner)))
+            {
+                var doctor = await _context.Doctors
+                    .FirstOrDefaultAsync(d => d.UserId == user.Id && !d.IsDeleted, cancellationToken);
+
+                if (doctor is not null)
+                {
+                    doctor.MarkAsDeleted(_currentUserService.UserId.ToString());
+                }
+            }
+
+            await _context.SaveChangesAsync(cancellationToken);
 
             return true;
         }
